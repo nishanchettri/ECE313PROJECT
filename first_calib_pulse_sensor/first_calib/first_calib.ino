8e757fda55d7a7a2c62ad8279984794749bc9d53
nishanchettri <35814272+nishanchettri@users.noreply.github.com>
2018-03-28 02:58:09 +0530

    Add files via upload

---
+++ first_calib_pulse_sensor/first_calib/first_calib.ino
@@ -0,0 +1,45 @@
+bool bpmTiming = false;
+bool beatComplete = false;
+int lastTime=0;
+int bpm=0;
+#define upperTh 550 //upper threshlold
+#define lowerTh 500 // lower threshhold
+void bpmCalc()
+{
+
+ int value= analogRead(0);
+  if (value>upperTh)
+  {
+    if(beatComplete)
+    {
+      bpm= millis()-lastTime;
+      bpm= int(60/(float(bpm)/1000));
+      bpmTiming=false;
+      beatComplete=false;
+    }
+    if(bpmTiming == false)
+    {
+      lastTime= millis();
+      bpmTiming= true;
+    }
+    
+  }
+  if((value<lowerTh)&(bpmTiming))
+  {
+    beatComplete=true;
+  }
+  Serial.println(bpm);
+  Serial.println(value);
+  delay(1000);
+}
+
+void setup()
+{
+  Serial.begin(112500);
+}
+
+void loop()
+{
+ bpmCalc();
+}
+
